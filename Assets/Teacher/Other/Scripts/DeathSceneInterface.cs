9edfcbcbd85a36c8229d85bc12e11948b1a7fd13
lol <lol@lol.com>
2016-01-29 15:17:19 +0100

    first

---
+++ Assets/Teacher/Other/Scripts/DeathSceneInterface.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Teacher
+{
+    public class DeathSceneInterface : MonoBehaviour
+    {
+
+        public void LoadLevel()
+        {
+            // Loader scenen med index 0
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Teacher/Other/Scripts/FoodObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Teacher
+{
+    public class FoodObject : MonoBehaviour
+    {
+        public float FoodAmount = 0.5f;
+
+        public AudioClip EatingSound;
+
+        void OnTriggerEnter(Collider ObjectThatTouchedMe)
+        {
+            if (ObjectThatTouchedMe.tag == "Player")
+            {
+                ObjectThatTouchedMe.GetComponent<Hunger>().HungerBar.value += FoodAmount;
+
+                AudioSource.PlayClipAtPoint(EatingSound, ObjectThatTouchedMe.transform.position);
+
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
+}
